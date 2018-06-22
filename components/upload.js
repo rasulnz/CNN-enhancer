e6b63804d756f2b01bb5c42265eb9a8028b1cc31
Rasul Niyazimbetov <Rasul.Niyazimbetov@albelli.com>
2018-06-22 10:32:05 +0200

    added trained libraries

--- components/upload.js
+++ components/upload.js
@@ -1,11 +1,14 @@
 const shellJs = require("shelljs");
 
-const handleUpload = (file, body) => {
+const handleUpload = (file) => {
 
     console.log(file);
 
     return new Promise(resolve => {
         resolve(true);
+
+        // const execStr = `enhance --zoom=2 --mode=default --type=photo ../public/${file.filename}`;const execStr = `ls`;
+        shellJs.exec(execStr);
     });
 };
 
